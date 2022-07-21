96baee4429d830d51640ed4d4046ba9c2bc9944b
Stavski <fernando.stavski@gmail.com>
2022-07-20 23:41:15 -0300

    feat: Iniciado a analise de crédito

--- app/Http/Controllers/CreditAnalysisController.php
+++ app/Http/Controllers/CreditAnalysisController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CreditAnalysis;
 
 class CreditAnalysisController extends Controller
 {
-    //
+    public function analiseDeCredito(Request $request)
+    {
+        dd( $request );
+    }
 }
--- app/Models/CreditAnalysis.php
+++ app/Models/CreditAnalysis.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class CreditAnalysis extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome', 'cpf', 'negativado', 'salario', 'limite_cartao', 'valor_aluguel', 'rua', 'numero', 'municipio', 'unidade_federativa', 'cep', 'created_at', 'updated_at'
+    ];
 }
